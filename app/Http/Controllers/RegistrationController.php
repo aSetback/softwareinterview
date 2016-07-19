7af96b28a74b89a66cd3b83a973e933333bf5ffb
Dave DeHaan <dave@krushcom.com>
2016-07-18 20:32:02 -0400

    DRY'd up the controller

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -9,69 +9,54 @@ use Session;
 
 class RegistrationController extends Controller
 {
+
+    private $rules = [
+        'first_name'    => 'required|max:100',
+        'last_name'     => 'required|max:100',
+        'address1'      => 'required|max:255',
+        'address2'      => 'max:255',
+        'city'          => 'required|max:100',
+        'state'         => 'required|max:2|alpha',
+        'country'       => 'required|max:100',
+        'zip'           => 'required|max:10|alpha_dash|min:5',
+    ];
+
     public function getIndex()
     {
         // Retrieve any errors we have ..
         $errors = Session::get('errors', []);
 
-        // This array is used if a user hits 'back' on the confirmation, so they don't have to re-enter everything.
-        $input = [
-            'first_name' => Input::get('first_name'),
-            'last_name'  => Input::get('last_name'),
-            'address1'  => Input::get('address1'),
-            'address2'  => Input::get('address2'),
-            'city'      => Input::get('city'),
-            'state'     => Input::get('state'),
-            'country'   => Input::get('country'),
-            'zip'       => Input::get('zip'),
-        ];
+        $input = $this->getInput();
 
         return view('registration.index', ['input' => $input, 'errors' => $errors]);
     }
 
     public function postConfirmation()
     {
-        // I'm defining each one explicitly to avoid having possible issues with undefined properties in the view.
-        $input = [
-            'first_name' => Input::get('first_name'),
-            'last_name'  => Input::get('last_name'),
-            'address1'  => Input::get('address1'),
-            'address2'  => Input::get('address2'),
-            'city'      => Input::get('city'),
-            'state'     => Input::get('state'),
-            'country'   => Input::get('country'),
-            'zip'       => Input::get('zip'),
-        ];
+        $input = $this->getInput();
+
+        // Validate our input according to the above rules
+        $validate = Validator::make($input, $this->rules);
+
+        if (!$validate->passes()) {
+
+            // Flash the errors to the session
+            Session::flash('errors', $validate->errors());
+
+            // Send the user back to the form, with their input in the query string to repopulate it.
+            return redirect()->route('registration.index', $input);
+
+        }
 
         return view('registration.confirmation', ['input' => $input]);
     }
 
     public function postSubmit()
     {
-        $input = [
-            'first_name' => Input::get('first_name'),
-            'last_name'  => Input::get('last_name'),
-            'address1'  => Input::get('address1'),
-            'address2'  => Input::get('address2'),
-            'city'      => Input::get('city'),
-            'state'     => Input::get('state'),
-            'country'   => Input::get('country'),
-            'zip'       => Input::get('zip'),
-        ];
+        $input = $this->getInput();
 
-        $rules = [
-            'first_name'    => 'required|max:100',
-            'last_name'     => 'required|max:100',
-            'address1'      => 'required|max:255',
-            'address2'      => 'max:255',
-            'city'          => 'required|max:100',
-            'state'         => 'required|max:50|alpha',
-            'country'       => 'required|max:100',
-            'zip'           => 'required|max:10|alpha_dash|min:5',
-        ];
-
-        // Validate our input according to the above rules
-        $validate = Validator::make($input, $rules);
+        // Re-validate, just in case.
+        $validate = Validator::make($input, $this->rules);
 
         // If it passes ..
         if ($validate->passes()) {
@@ -90,6 +75,19 @@ class RegistrationController extends Controller
     public function getThanks()
     {
         return view('registration.thanks');
+    }
+
+    private function getInput() {
+        return [
+            'first_name' => Input::get('first_name'),
+            'last_name'  => Input::get('last_name'),
+            'address1'  => Input::get('address1'),
+            'address2'  => Input::get('address2'),
+            'city'      => Input::get('city'),
+            'state'     => Input::get('state'),
+            'country'   => 'US', // On the front end, 'US', 'USA', and 'United States are valid'.  Standardizing.
+            'zip'       => Input::get('zip'),
+        ];
     }
 }
 
